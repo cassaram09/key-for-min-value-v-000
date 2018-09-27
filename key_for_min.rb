2c4074d4f4591f251568a8113d3cfddba27a5452
cassaram09 <cassaram09@gmail.com>
2018-09-27 14:51:06 -0400

    break lab

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hash)
+  lowest_key = ''
+  lowest_value = 0
 
+  hash.each do |k, v|
+    if  v < lowest_value
+      lowest_value = v
+    end
+  end
 end
